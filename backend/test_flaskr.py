218aa3f1ebbaae038865ce4728ef60705cd3ea8c
Chris Brandt <xtopher.brandt@gmail.com>
2023-09-02 11:41:05 -0700

    fix: correct database path and use app context

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,11 +12,12 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}:{}@{}/{}".format('xtopher','wolf','localhost:5432', self.database_name)
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+        with self.app.app_context():
+            setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
