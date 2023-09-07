35e0c07d887abd4afc983262ba73c8bc9b5a867d
Chris Brandt <xtopher.brandt@gmail.com>
2023-09-07 15:57:19 -0700

    fix: return categories in format expected by frontend

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,16 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -17,19 +27,26 @@ def create_app(test_config=None):
         else:
             setup_db(app, test_config)
 
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
+    CORS(app)
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
     
     @app.route('/categories', methods=['GET'])
     def get_categories():
         categories = Category.query.all()
-        categories_json_formated = [ category.format() for category in categories ]
+        categories_json_formated = {}
+        for category in categories:
+            categories_json_formated[category.id] = category.type
         return jsonify({
             "categories": categories_json_formated, 
             "success": True
@@ -48,7 +65,22 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
-
+    @app.route('/questions', methods=['GET'])
+    def get_questions_no_page_specified():
+        questions = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, questions)
+        categories = Category.query.all()
+        categories_json_formated = {}
+        for category in categories:
+            categories_json_formated[category.id] = category.type
+        
+        return jsonify({
+            "categories": categories_json_formated,
+            "questions": current_questions,
+            "current_category": categories_json_formated[1],
+            "total_questions": len(questions),
+            "success": True
+        })
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -21,28 +21,92 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+    def check_basic_response_format(self, result, response_keys = [] ):
+        
+        result_data_json = json.loads( result.data )
+        self.assertTrue('success' in result_data_json.keys(), 'Incorrect response format. Missing "success" key' )       
+
+        if int( result.status_code / 100 ) == 2 :
+            self.assertEqual( result_data_json['success'], True, f'Incorrect response message. Status code == {result.status_code}, response message not True' ) 
+            for key in response_keys:
+                self.assertIn( key, result_data_json, f'Incorrect response message. Missing the key: {key}' )
+        else:
+            self.assertEqual( result_data_json['success'], False, f'Incorrect response message. Status code == {result.status_code}, response message not False' )
+            self.assertEqual( result_data_json['error'], result.status_code, f'HTTP response status code {result.status_code} does not match response message error code {result_data_json["error"]}')
+            self.assertIn('message', result_data_json, 'Incorrect response message, missing error message')
+        return
+            
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-    def test_get_categories_returns_success_and_categories(self):
+    def test_get_categories_returns_all_categories(self):
         result = self.client().get('/categories')
         
+        data_json = json.loads( result.data )
+                
         self.assertEqual( result.status_code, 200 )
-        self.assertTrue('categories' in json.loads( result.data).keys())
-        self.assertGreater( len(json.loads( result.data)['categories']), 0 )
+        self.check_basic_response_format( result, ['categories'] )
+        self.assertGreater( len(data_json['categories']), 0 )
+        
+    def test_categories_are_returned_in_correct_format(self):
+        result = self.client().get('/categories')
+        
+        data_json = json.loads( result.data )
+                
+        self.assertEqual( result.status_code, 200 )
+        self.check_basic_response_format( result, ['categories'] )
+        self.assertEqual( type(data_json['categories']), dict, "Incorrect response format. GET Categories did not return a dict.")
 
     def test_get_categories_returns_method_not_allowed_for_POST(self):
         result = self.client().post('/categories')
         self.assertEqual( result.status_code, 405 )
-        self.assertEqual({
-            'success': False,
-            'error': 405,
-            'message': 'Method not allowed.'
-        }, json.loads(result.data))
+        self.check_basic_response_format( result )
         
-    
+    def test_get_questions_without_page_returns_first_page_of_questions(self):
+        result = self.client().get('/questions')
+        
+        self.assertEqual( result.status_code, 200 )
+        self.check_basic_response_format( result, ['categories', 'questions', 'current_category', 'total_questions'] )
+        
+        questions_returned_no_page_number = json.loads( result.data )['questions']
+        
+        result_2 = self.client().get('/questions?page=1')
+        questions_returned_page_1 = json.loads( result_2.data )['questions']
+        
+        question_index = 0
+        for question_no_page in questions_returned_no_page_number:
+            question_page_1 = questions_returned_page_1[question_index]
+            self.assertDictEqual(question_no_page, question_page_1, 'GET questions without a page query did not return the same set as GET questions with a page query')
+            question_index += 1
 
+    def test_get_questions_page_2_returns_a_set_of_questions_after_page_1(self):
+        result_2 = self.client().get('/questions?page=2')
+        
+        data_json_2 = json.loads( result_2.data )
+        
+        self.assertEqual( result_2.status_code, 200 )
+        self.check_basic_response_format( result_2, ['questions'] )
+        
+        id_of_first_question = data_json_2['questions'][0]['id']
+        
+        result_1 = self.client().get('/questions?page=1')
+        data_json_1 = json.loads( result_1.data )
+        id_of_last_question = data_json_1['questions'][-1]['id']
+        
+        self.assertGreater(id_of_first_question, id_of_last_question, 'First question of second page is not greater than last question of first page.')
+
+    def test_get_questions_without_category_sets_current_category_to_a_valid_category(self):
+        result = self.client().get('/questions')
+        
+        data_json = json.loads( result.data )
+        
+        self.assertEqual( result.status_code, 200 )
+        self.check_basic_response_format( result, ['categories', 'questions', 'current_category', 'total_questions'] )
+        
+        current_category = data_json['current_category']
+        self.assertNotEqual( current_category, None, 'Incorrect response format. Current category is set to None')
+        
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
